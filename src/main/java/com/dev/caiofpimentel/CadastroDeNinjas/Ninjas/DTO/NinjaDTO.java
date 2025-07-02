81bb13d963ff0958f25467b2b7b5a5b889caf53f
Caio <caio-pimentel@hotmail.com>
2025-07-02 19:07:39 -0300

    feat: Cria mapper e DTO para entidade Ninja.

---
+++ src/main/java/com/dev/caiofpimentel/CadastroDeNinjas/Ninjas/DTO/NinjaDTO.java
@@ -0,0 +1,4 @@
+package com.dev.caiofpimentel.CadastroDeNinjas.Ninjas.DTO;
+
+public class NinjaDTO {
+}
---
+++ src/main/java/com/dev/caiofpimentel/CadastroDeNinjas/Ninjas/mapper/NinjaMapper.java
@@ -0,0 +1,4 @@
+package com.dev.caiofpimentel.CadastroDeNinjas.Ninjas.mapper;
+
+public class NinjaMapper {
+}
