e0f691fcd61aa5b6ba9e1ada0b08508d0a7d20d6
Caio <caio-pimentel@hotmail.com>
2025-07-03 15:18:18 -0300

    Chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidadesxDTO

--- src/main/java/com/dev/caiofpimentel/CadastroDeNinjas/Ninjas/DTO/NinjaDTO.java
+++ src/main/java/com/dev/caiofpimentel/CadastroDeNinjas/Ninjas/DTO/NinjaDTO.java
@@ -1,4 +1,22 @@
 package com.dev.caiofpimentel.CadastroDeNinjas.Ninjas.DTO;
 
+
+import com.dev.caiofpimentel.CadastroDeNinjas.Missoes.model.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissaoModel missoes;
+
 }
